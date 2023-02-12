649182a7b40ac3e090ae88caaa996a1c08424b2d
Tico <Tico@3143242887.com>
2023-02-12 15:47:41 +0800

    hot-fix commit2

--- src/main/java/com/gdtico/git/GitTest.java
+++ src/main/java/com/gdtico/git/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello Git");
         System.out.println("hello Git");
         System.out.println("hello Git");
+        System.out.println("hello Git");
     }
 
 }
