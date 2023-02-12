11fb9370ca0e96aa98f50bf01f7c6db9d5a0f6cf
Tico <Tico@3143242887.com>
2023-02-12 16:08:14 +0800

    push commit

--- src/main/java/com/gdtico/git/GitTest.java
+++ src/main/java/com/gdtico/git/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello Git");
         System.out.println("hello Git");
         System.out.println("master test");
+        System.out.println("push test");
     }
 
 }
