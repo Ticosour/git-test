b0be07e51b16e2a48770be256fbb962947cd6f66
Tico <Tico@3143242887.com>
2023-02-12 11:11:59 +0800

    second commit

--- src/main/java/com/gdtico/git/GitTest.java
+++ src/main/java/com/gdtico/git/GitTest.java
@@ -3,7 +3,7 @@ package com.gdtico.git;
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("hello Git");
+        System.out.println("hello Git22");
     }
 
 }
